2ec174950aaa1976a6c597f8b78c74b865014183
Jordy Webb <jordy@thegrid.io>
2017-05-11 06:03:09 -0700

    add support for CHANGE_NAME action

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -4,9 +4,28 @@ console.log('starting redux example');
 
 var reducer = (state = {name: 'Annonymous'}, action) => {
   //state = state || {name: 'Annonymous'};
-  return state;
+
+  console.log('New action:', action);
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+      default:
+        return state;
+  }
+
 };
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Jordy'
+};);
+
+console.log('Name should be Jordy', store.getState());
--- app/redux-todo-example.jsx
+++ app/redux-todo-example.jsx
@@ -8,10 +8,28 @@ var stateDefault = {
   todos: []
 }
 var reducer = (state = stateDefault, action) => {
-  return state;
+
+  console.log('New Action', action);
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      };
+      default:
+        return state;
+  }
+
 };
 
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log('currentState', currentState);
+
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'New search text'
+});
+
+console.log('Search text should be new search text', store.getState());

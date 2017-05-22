bbaadb6606de00b8c73c9866f7d69d0c4ee37183
Jordy Webb <jordy@thegrid.io>
2017-05-22 07:25:53 -0700

    add actions for removing from arrays

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -31,6 +31,11 @@ var reducer = (state = stateDefault, action) => {
           }
         ]
       };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      }
     case 'ADD_MOVIE':
       return {
         ...state,
@@ -42,6 +47,11 @@ var reducer = (state = stateDefault, action) => {
             genre: action.genre
           }
         ]
+      }
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
       }
       default:
         return state;
@@ -76,9 +86,30 @@ store.dispatch({
 });
 
 store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Paddling'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
   type: 'ADD_MOVIE',
   title: 'Pulp Fiction',
   genre: 'Indy'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Endless Summer',
+  genre: 'Doc'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 2
 });
 
 store.dispatch({
